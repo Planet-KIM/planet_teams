d0356863c297791da6936fd324cd167efa0575c8
uiju <qkrdmlwnk@naver.com>
2020-07-17 20:43:41 +0900

    test

---
+++ uiju.py
@@ -0,0 +1,3 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>>import pymsql
